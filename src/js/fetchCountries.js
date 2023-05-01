61a2859ecdcdd5a87ea6c1db5752d3d1e2501b68
Deimer23 <116132047+Deimer23@users.noreply.github.com>
2023-05-01 05:12:23 +0000

    Deploying to gh-pages from @ Deimer23/goit-js-hw-10@fdedec7b9c936ae19d955265ca693a34217abd7b 🚀

--- src/js/fetchCountries.js
+++
@@ -1,6 +0,0 @@
-
-export function fetchCountries(){
-    return fetch("https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages")
-              .then((countries) =>countries.json());
-}
-
